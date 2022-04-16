05bec20e3cf47dcfdef60bbcf7b3f2379b386dcd
nethaji26 <103849020+nethaji26@users.noreply.github.com>
2022-04-17 01:00:17 +0530

    Add files via upload

---
+++ LearnDataTypes.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+import org.openqa.selenium.WebDriver;
+
+public class LearnDataTypes {
+	
+	boolean isCarNew = true;
+	byte maxSpeed = 120;
+	char model ='x';
+	short price = 32000;
+	int fuelCapacity  = 50;
+	long kmRun = 250005555555L;
+	float musicPlayerPrice = 985.32F;
+	double tyrePrice = 1500.521245666;
+	public static void main(String[] args) {
+		System.out.println(100+5);
+		WebDriver driver = new1 ;
+		
+	}
+	
+
+}
